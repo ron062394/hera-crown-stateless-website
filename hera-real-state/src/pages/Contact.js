4590693410f55e589da5a93d7f7d05520f8af7dc
gabster <gabster@yolo.com>
2023-12-15 19:59:55 +0800

    added margins and nav animation

--- hera-real-state/src/pages/Contact.js
+++ hera-real-state/src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <h2>Get in Touch with Hera's Dream Dwellings</h2>
             <div>
                 <h3>Address:</h3>
