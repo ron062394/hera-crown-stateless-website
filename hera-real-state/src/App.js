2529bb423a4099962bd5f6f66e181babe84fdb59
Regel <regelcabrales@gmail.com>
2023-12-15 23:52:37 +0800

    Changed properties

--- hera-real-state/src/App.js
+++ hera-real-state/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>              
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/properties' element={<Properties/>}/>
--- hera-real-state/src/pages/Properties.js
+++ hera-real-state/src/pages/Properties.js
@@ -13,7 +13,7 @@ const Properties = () => {
                         <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Orchid Casel de Paradise</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -25,10 +25,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://www.realestate.com.au/news-image/w_2000,h_1500/v1666226657/news-lifestyle-content-assets/wp-content/production/image-10_787815ed2d9.jpg?_i=AA" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>The Contempo</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -40,10 +40,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://images.ctfassets.net/n2ifzifcqscw/3QRMlAcJFrYAEAbhziixZW/d4b9aa50215c5ea7a161b8a6b59f1974/hero-real-estate-facts-trends.jpeg" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Pacaso Roque</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -55,10 +55,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-efnC_54GPGI/XU_382v9tzI/AAAAAAAAUug/ogPWFEJZUzMNQUuwtZcPcKLaaYw4SiYTQCLcBGAs/s640/Beautiful%2BHomes%2Bin%2BLos%2BAngeles%252C%2BCalifornia%2B1.jpg" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Franklin Avenue</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -70,10 +70,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://insidecolumbia.net/wp-content/uploads/2019/11/Exterior-Photos-3-copy_featuredImage.jpg" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>The Dove</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -85,10 +85,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://netstorage-briefly.akamaized.net/images/9bb6736637e233e1.jpg" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Light, Water, and Geometry</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -100,10 +100,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://w0.peakpx.com/wallpaper/334/799/HD-wallpaper-beautiful-house-architecture-beautiful-house-houses.jpg" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Comfort Peak</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -115,10 +115,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19fKRajH-mnilbow5q5TPXCaj5jPO2_eS4g&usqp=CAU" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>Royal Holiday</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
@@ -130,10 +130,10 @@ const Properties = () => {
 
                 <div class="property-wrapper">
                     <div class="img-container">
-                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                        <img src="https://www.narcity.com/media-library/image.jpg?id=25959902&width=1245&height=700&coordinates=0%2C52%2C0%2C52" alt="" />
                     </div>
                     <div class="property-body">
-                        <h2>Orchel Casel de Paradise</h2>
+                        <h2>The Glass Mansion</h2>
                         <p>
                         123 St. San Francisco, California.
                         </p>
