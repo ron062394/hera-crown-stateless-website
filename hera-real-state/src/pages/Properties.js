e026c0ad62a4b06c39cc96554b040e711f1c42b7
Regel <regelcabrales@gmail.com>
2023-12-15 19:48:24 +0800

    Added Property cards

--- hera-real-state/src/pages/Properties.js
+++ hera-real-state/src/pages/Properties.js
@@ -1,14 +1,69 @@
 import React from "react";
+import "./styles/Properties.css";
 
 const Properties = () => {
     return (
         <div>
-            
+            <div class="title">
+                <h2>Best Properties</h2>
+            </div>
+            <section class="properties">
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                        <a href="#" class="read-more">
+                        View Property
+                        </a>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
 
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                        <a href="#" class="read-more">
+                        View Property
+                        </a>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
 
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                        <a href="#" class="read-more">
+                        View Property
+                        </a>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
-
 
 export default Properties;
