571640cddcb077c54caa0bb563d6ab3f3d34c97e
Ron <ron062394@gmail.com>
2023-12-15 19:01:28 +0800

    Added hero image

--- hera-real-state/src/pages/Home.js
+++ hera-real-state/src/pages/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
   return (
     <div>
       <section className="main">
-        <h1>Where Homes Find Their Stories</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
-          maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
-          facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
-        </p>
-        <img src="" alt="" />
+        <div>
+            <h1>Where Homes Find Their Stories</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
+            maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
+            facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
+            </p>
+        </div>
+        <img src="https://i.postimg.cc/6QvHKDsh/view-of-modern-business-skyscrapers-glass-and-sky-view-landscape-of-commercial-building-in-central-c.png" alt="building-condominium" />
       </section>
     </div>
   );
