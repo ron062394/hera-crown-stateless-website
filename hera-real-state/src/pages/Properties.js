f5033af832ea56ddb4b408b4558cc34536482e16
Regel <regelcabrales@gmail.com>
2023-12-15 20:10:48 +0800

    Added more property cards

--- hera-real-state/src/pages/Properties.js
+++ hera-real-state/src/pages/Properties.js
@@ -3,9 +3,9 @@ import "./styles/Properties.css";
 
 const Properties = () => {
     return (
-        <div>
+        <div class="property">
             <div class="title">
-                <h2>Best Properties</h2>
+                <h2>Our Properties</h2>
             </div>
             <section class="properties">
                 <div class="property-wrapper">
@@ -17,9 +17,6 @@ const Properties = () => {
                         <p>
                         123 St. San Francisco, California.
                         </p>
-                        <a href="#" class="read-more">
-                        View Property
-                        </a>
                     </div>
                     <div class="for-sale-tag">
                         For Sale
@@ -35,9 +32,6 @@ const Properties = () => {
                         <p>
                         123 St. San Francisco, California.
                         </p>
-                        <a href="#" class="read-more">
-                        View Property
-                        </a>
                     </div>
                     <div class="for-sale-tag">
                         For Sale
@@ -53,9 +47,96 @@ const Properties = () => {
                         <p>
                         123 St. San Francisco, California.
                         </p>
-                        <a href="#" class="read-more">
-                        View Property
-                        </a>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
+                    </div>
+                    <div class="for-sale-tag">
+                        For Sale
+                    </div>
+                </div>
+
+                <div class="property-wrapper">
+                    <div class="img-container">
+                        <img src="https://upavp.project247.in/assets/images/properties/properties1.png" alt="" />
+                    </div>
+                    <div class="property-body">
+                        <h2>Orchel Casel de Paradise</h2>
+                        <p>
+                        123 St. San Francisco, California.
+                        </p>
                     </div>
                     <div class="for-sale-tag">
                         For Sale
