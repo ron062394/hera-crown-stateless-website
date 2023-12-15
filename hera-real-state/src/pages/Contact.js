d3bb31d9cfb372bfc186a83ef9fa90c8c461f220
Ron <ron062394@gmail.com>
2023-12-15 18:22:54 +0800

    Added contact us contents

--- hera-real-state/src/pages/Contact.js
+++ hera-real-state/src/pages/Contact.js
@@ -3,7 +3,29 @@ import React from "react";
 const Contact = () => {
     return (
         <div>
-            Contact
+            <h2>Get in Touch with Hera's Dream Dwellings</h2>
+            <div>
+                <h3>Address:</h3>
+                <p>123 Dream Home Street,
+Cityville, State, Zip Code</p>
+            </div>
+            <div>
+                <h3>Phone:</h3>
+                <p>Main Office: +1 (123) 456-7890</p>
+                <p>Sales Inquiries: +1 (987) 654-3210</p>
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
         </div>
     )
 }
