6399f78229168670a5c47f529e1015d1f94fa38b
gabster <gabster@yolo.com>
2023-12-16 11:35:17 +0800

    fixed background image

--- hera-real-state/src/pages/Home.js
+++ hera-real-state/src/pages/Home.js
@@ -3,17 +3,15 @@ import "./styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-div">
       <section className="main">
-        <div>
             <h1>Where Homes Find Their Stories</h1>
-            <p>
+            <p className="homeP">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
             maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
             facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
             </p>
-        </div>
-        <img src="https://i.postimg.cc/6QvHKDsh/view-of-modern-business-skyscrapers-glass-and-sky-view-landscape-of-commercial-building-in-central-c.png" alt="building-condominium" />
+        {/* <img src="https://i.postimg.cc/6QvHKDsh/view-of-modern-business-skyscrapers-glass-and-sky-view-landscape-of-commercial-building-in-central-c.png" alt="building-condominium" />  */}
       </section>
     </div>
   );
