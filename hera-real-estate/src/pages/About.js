97a14ea3e60898ef421f28361f0420bd63ab192c
ron062394 <118293091+ron062394@users.noreply.github.com>
2024-02-09 10:30:14 +0800

    Update About.js

--- hera-real-estate/src/pages/About.js
+++ hera-real-estate/src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <h2>Welcome to Hera's Dream Dwellings</h2>
                 <p>At Hera's Dream Dwellings, we believe that finding the perfect home is not just about the property; it's about discovering the place where your dreams take root and flourish. Established in [year], our journey began with a passion for helping individuals and families find their ideal homes, and it continues with an unwavering commitment to excellence in real estate services.</p>
             </div>
-            <div>
+            <div className="mission-container">
                 <h2>Our mission</h2>
                 <p>Our mission is simple yet profound: to guide and support you through every step of your real estate journey. We strive to create meaningful connections between people and properties, transforming houses into homes and aspirations into realities.</p>
             </div>
